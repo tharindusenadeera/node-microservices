d5433e3af68a07a4f39e1602389242a78eed14ee
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-08-19 22:00:40 +0100

    Creating and testing registration routes done

--- workspace/microservices/registry-service/routes/index.js
+++ workspace/microservices/registry-service/routes/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const Registry = require("../lib/Registry");
 
 const router = express.Router();
+const registry = new Registry();
 
 router.put(
   "/register/:servicename/:serviceversion/:serviceport",
   (req, res, next) => {
-    return next("Not implemented");
+    const { servicename, serviceversion, serviceport } = req.params;
+    const serviceip = req.ip;
+    if (serviceip.includes("::1") || serviceip.includes("::ffff:127.0.0.1")) {
+      serviceip = "127.0.0.1";
+    }
+    const key = registry.register(
+      servicename,
+      serviceversion,
+      serviceip,
+      serviceport
+    );
+    return res.json({ result: key });
   }
 );
 
